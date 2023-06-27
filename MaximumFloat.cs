d68a9e8c93a1aab55c67c1232ac80d0a2bc7d7e9
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-27 09:10:34 +0530

    [Shivam Tripathi]: Added Generics Problem Find Maximum Float

---
+++ MaximumFloat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    internal class MaximumFloat
+    {
+        public static float FindMaxFloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default(float);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,16 @@ namespace GenericsDemo2
     {
         static void Main(string[] args)
         {
-            int result = MaximumInteger.FindMaxInt(22, 45, 60);
-            Console.WriteLine("Maximum number is:" + result);
 
+            // ------------ UC 1----------------------
+            //int result = MaximumInteger.FindMaxInt(22, 45, 60);
+            //Console.WriteLine("Maximum number is:" + result);
+
+
+
+            // -----------------------UC2-------------------------
+            float result1 = MaximumFloat.FindMaxFloat(22.4f, 44.6f, 63.6f);
+            Console.WriteLine("Maximum Float Number is: " + result1);
         }
     }
 }
