03f24da2e2e8d8352d4448f5b2c51fd13d04d350
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-27 09:17:18 +0530

    [Shivam Tripathi]: Added Generics Problem Find Maximum string

---
+++ MaximumString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    internal class MaximumString
+    {
+        public static string FindMaxString(string name1, string name2, string name3)
+        {
+            if (name1.CompareTo(name2) > 0 && name1.CompareTo(name2) > 0 ||
+
+               name1.CompareTo(name2) >= 0 && name1.CompareTo(name2) > 0 ||
+
+               name1.CompareTo(name2) > 0 && name1.CompareTo(name2) >= 0)
+            {
+                return name1;
+            }
+            if (name2.CompareTo(name1) > 0 && name2.CompareTo(name3) > 0 ||
+
+               name2.CompareTo(name1) >= 0 && name2.CompareTo(name3) > 0 ||
+
+               name2.CompareTo(name1) > 0 && name2.CompareTo(name3) >= 0)
+            {
+                return name2;
+            }
+            if (name3.CompareTo(name1) > 0 && name3.CompareTo(name2) > 0 ||
+
+               name3.CompareTo(name1) >= 0 && name3.CompareTo(name2) > 0 ||
+
+               name3.CompareTo(name1) > 0 && name3.CompareTo(name2) >= 0)
+            {
+                return name3;
+            }
+            return default;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,8 +14,15 @@ namespace GenericsDemo2
 
 
             // -----------------------UC2-------------------------
-            float result1 = MaximumFloat.FindMaxFloat(22.4f, 44.6f, 63.6f);
-            Console.WriteLine("Maximum Float Number is: " + result1);
+            //float result1 = MaximumFloat.FindMaxFloat(22.4f, 44.6f, 63.6f);
+            //Console.WriteLine("Maximum Float Number is: " + result1);
+
+
+
+            // ---------------------------UC3 --------------------------
+
+            string Result2 = MaximumString.FindMaxString("Shivam", "Divya", "Aakanksha");
+            Console.WriteLine("Maximum String is:" + Result2);
         }
     }
 }
