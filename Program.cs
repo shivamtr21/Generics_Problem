ddf5a3ba8aa7d6b3a6f61d2d7a1ee1d4c8340fa5
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-27 09:04:11 +0530

    [Shivam Tripathi]: Added Generics Problem Find Maximum Integer

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericsAssignment;
+
+namespace GenericsDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = MaximumInteger.FindMaxInt(22, 45, 60);
+            Console.WriteLine("Maximum number is:" + result);
+
+        }
+    }
+}
